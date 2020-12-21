e48462e3303e2696ceaa5984dc33129ddb796b37
baldo-vela <baldo.vela+github@gmail.com>
2020-12-21 10:03:40 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+    # Associate the Actor model with the Character and Show model. An actor should have many characters and many shows through characters.
+    # Write a method in the Actor class, #full_name, that returns the first and last name of an actor.
+    has_many :characters
+    has_many :shows, through: :characters
+    
+    # Write a method in the Actor class, #list_roles, that lists all of the characters that actor has alongside the show that the character is in. 
+
+    def list_roles
+        self.characters.map do |c|
+            "#{c.name} - #{c.show.name}"
+        end
+    end
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    
 end
